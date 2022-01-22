fc7c8771aa6219109acc7ca260de9caadcdccd28
ErikPC <epardillo@cifpfboll.eu>
2022-01-22 20:51:37 +0100

    refactor act2_5

--- actividades javascript/act2/act2_5.js
+++ actividades javascript/act2/act2_5.js
@@ -2,8 +2,8 @@ function mas_grande(){
     let numero_uno = prompt('dime un numero');
     let numero_dos = prompt('dime otro numero');
     if (numero_uno > numero_dos) {
-        alert(numero_uno)
+        window.alert(numero_uno)
     } else {
-        alert(numero_dos)
+        window.alert(numero_dos)
     }
 }
