f206ebcbe02e4500fb63edbef22e7422ba1e11b8
ErikPC <epardillo@cifpfbmoll.eu>
2022-01-28 17:16:40 +0100

    finalizar act4_4

--- actividades javascript/act4/act4_4.js
+++ actividades javascript/act4/act4_4.js
@@ -1,3 +1,6 @@
-function aumentarlink(){
-    document.getElementById("link").addEventListener()
+function bigLink(x){
+    x.style.fontSize = "2em"
+}
+function normalLink(x){
+    x.style.fontSize = "1em"
 }
