603b91367914b30373de9d9c522d5993a58de137
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-08 19:27:46 +0100

    finalizar act_1 y index con botones para comprobar que funcionan

--- actividades javascript/Practica2/act_1.js
+++ actividades javascript/Practica2/act_1.js
@@ -2,9 +2,12 @@ function pedir_numeros() {
   let lista_numeros = [];
   let counter = 0;
   while (counter <= 9) {
-    let numero = prompt("Dime un numero");
+    let numero = parseInt(prompt("Dime un numero"));
     lista_numeros[counter] = numero;
     counter++;
   }
-  window.alert(lista_numeros);
+  var ordenada = lista_numeros.sort((a, b) => {
+    return a - b;
+  });
+  window.alert(ordenada);
 }
