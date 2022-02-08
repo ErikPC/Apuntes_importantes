18cc315db123c0f5265d42e79297b559314c5c39
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-08 13:50:11 +0100

    crear html y act_8 js

---
+++ actividades javascript/Practica2/act_8.js
@@ -0,0 +1,18 @@
+function colorAzul() {
+  document.body.style.backgroundColor = "#33BBFF";
+}
+function colorAmarillo() {
+  document.body.style.backgroundColor = "orange";
+}
+function colorMarron() {
+  document.body.style.backgroundColor = "#CD853F";
+}
+function colorRojo() {
+  document.body.style.backgroundColor = "red";
+}
+function colorVerde() {
+  document.body.style.backgroundColor = "green";
+}
+function colorNormal() {
+  document.body.style.backgroundColor = "white";
+}
