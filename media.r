207154dd445d61740c6fb468d4b24b30669de852
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-28 17:58:18 +0100

    .

--- media.r
+++ media.r
@@ -1,16 +1,16 @@
-PR = 3.5
-PT1 = 1.96
-MT1 = 3.15
-PT2 = 7
-MT2 = 7
-PJ = 6
+PR <- 3 # nolint
+PT1 <- 4.1 # nolint
+MT1 <- 3.7 # nolint
+PT2 <- 3.8 # nolint
+MT2 <- 3.7 # nolint
+PJ <- 9 # nolint
 
-regla_tres <- function(valor , perc) {
+regla_tres <- function(valor, perc) {
     valor * perc / 100
 }
-proyecto = regla_tres(PJ, 30)
-examen_practico = regla_tres(PR, 40)
-programacion = regla_tres((PT1 + PT2)/2,20 )
-metodologias = regla_tres((MT1 + MT2)/2,10 )
+proyecto <- regla_tres(PJ, 30)
+examen_practico <- regla_tres(PR, 40)
+programacion <- regla_tres((PT1 + PT2) / 2, 20)
+metodologias <- regla_tres((MT1 + MT2) / 2, 10)
 
-print(proyecto + examen_practico + programacion + metodologias) 
+print(proyecto + examen_practico + programacion + metodologias)
