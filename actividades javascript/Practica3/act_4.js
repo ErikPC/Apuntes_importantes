09a845c1091587ddf450f295b096fd55c68e457b
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-04 21:58:42 +0100

    fix: añadir regex al js y eliminarlo del html

--- actividades javascript/Practica3/act_4.js
+++ actividades javascript/Practica3/act_4.js
@@ -1,7 +1,11 @@
 function validar(form) {
   let contraseña = form.contraseña.value;
 
-  if (!regex.test(contraseña)) {
+  if (!/.{4,8}[0-9]{2}/.test(contraseña)) {
     window.alert("Contraseña invalida");
+  }
+
+  if (!/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/) {
+    window.alert("Mail invalido");
   }
 }
