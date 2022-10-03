00a4626b47e38ac306108fe84ff6f6280d2d62b0
ErikPC <epardillo@cifpfbmoll.eu>
2022-10-03 17:57:45 +0200

    josep: deja de copiar

--- actividades_js_2/act4/codigo.js
+++ actividades_js_2/act4/codigo.js
@@ -7,6 +7,33 @@ function isString(valor) {
     return false;
   }
 }
+function isBool(valor) {
+  if (typeof valor == "boolean") {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isInt(valor) {
+  if (typeof valor == "integer") {
+    return true;
+  } else {
+    return false;
+  }
+}
 var masGrande = valores
   .filter((valor) => isString(valor))
   .sort((s1, s2) => s1.length > s2.length)[0];
+
+var varTrue = valores.filter((valor) => isBool(valor));
+varTrue = varTrue[0] || varTrue[1];
+var varFalse = varTrue[0] && varTrue[1];
+
+var operaciones = valores.filter((valor) => isInt(valor));
+
+var resultado = 0;
+var suma = operaciones.map((valor) => resultado + valor);
+var resta = operaciones[0] - operaciones[1];
+var multiplicacion = operaciones[0] * operaciones[1];
+var division = operaciones[0] / operaciones[1];
+var modulo = operaciones[0] % operaciones[1];
