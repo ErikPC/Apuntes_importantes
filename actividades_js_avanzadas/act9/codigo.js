419f268e7ca5f65626948983834a4ad61276fe80
ErikPC <epardillo@cifpfbmoll.eu>
2022-10-20 15:57:51 +0200

    feat: subir

---
+++ actividades_js_avanzadas/act9/codigo.js
@@ -0,0 +1,14 @@
+function infoString(cadena) {
+  if (cadena.match(/[A-Z]+/)) {
+    this.alert("Esta hecha por mayusculas");
+  } else {
+    this.alert("Esta hecha por mayusculas");
+  }
+}
+var cadena = prompt("pikopiko");
+
+if (cadena.match(/[A-Z]+/)) {
+  this.alert("Esta hecha por mayusculas");
+} else {
+  this.alert("Esta hecha por mayusculas");
+}
