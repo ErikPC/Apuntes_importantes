e6ef4835a4a96ea5099d91c44c840753e6a1a1ac
ErikPC <epardillo@cifpfbmoll.eu>
2022-12-07 20:19:32 +0100

    feat: hacer actividad

---
+++ activitat_js_bloc_2/act8/codigo.js
@@ -0,0 +1,10 @@
+// salta un alert con el String
+
+function parOImpar(numero) {
+  var resultado = numero % 2;
+  if (resultado == 0) {
+    window.alert("es par");
+  } else {
+    window.alert("es impar");
+  }
+}
