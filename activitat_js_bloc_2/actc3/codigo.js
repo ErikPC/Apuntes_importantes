bc3cfb3bc1408f3fc2c69d748835d9b43f4be295
ErikPC <epardillo@cifpfbmoll.eu>
2022-12-08 12:26:02 +0100

    fix: hacer bien actc3

--- activitat_js_bloc_2/actc3/codigo.js
+++ activitat_js_bloc_2/actc3/codigo.js
@@ -1,20 +1,122 @@
-function getSignoZodiaco(dia, mes) {
-  if ((dia >= 21 && mes == 3) || (dia <= 20 && mes == 4)) console.log("Aries");
-  if ((dia >= 24 && mes == 9) || (dia <= 23 && mes == 10)) console.log("Libra");
-  if ((dia >= 21 && mes == 4) || (dia <= 21 && mes == 5)) console.log("Tauro");
-  if ((dia >= 24 && mes == 10) || (dia <= 22 && mes == 11))
-    console.log("Escorpio");
-  if ((dia >= 22 && mes == 5) || (dia <= 21 && mes == 6))
-    console.log("G\u00E9minis");
-  if ((dia >= 23 && mes == 11) || (dia <= 21 && mes == 12))
-    console.log("Sagitario");
-  if ((dia >= 21 && mes == 6) || (dia <= 23 && mes == 7))
-    console.log("C\u00E1ncer");
-  if ((dia >= 22 && mes == 12) || (dia <= 20 && mes == 1))
-    console.log("Capricornio");
-  if ((dia >= 24 && mes == 7) || (dia <= 23 && mes == 8)) console.log("Leo");
-  if ((dia >= 21 && mes == 1) || (dia <= 19 && mes == 2))
-    console.log("Acuario");
-  if ((dia >= 24 && mes == 8) || (dia <= 23 && mes == 9)) console.log("Virgo");
-  if ((dia >= 20 && mes == 2) || (dia <= 20 && mes == 3)) console.log("Piscis");
+function getSignoZodiaco(fecha) {
+  var dia = parseInt(fecha[0] + fecha[1]);
+  var mes = parseInt(fecha[3] + fecha[4]);
+  let aries = function (dia, mes) {
+    if (mes == 4 && dia <= 20) {
+      console.log("Aries");
+    }
+    if (mes == 3 && dia >= 21) {
+      console.log("Aries");
+    }
+  };
+  let libra = function (dia, mes) {
+    if (mes == 10 && dia <= 23) {
+      console.log("Libra");
+    }
+    if (mes == 9 && dia >= 24) {
+      console.log("Libra");
+    }
+  };
+  let tauro = function (dia, mes) {
+    if (mes == 5 && dia <= 21) {
+      console.log("Tauro");
+    }
+    if (mes == 4 && dia >= 21) {
+      console.log("Tauro");
+    }
+  };
+  let scorpio = function (dia, mes) {
+    if (mes == 11 && dia <= 22) {
+      console.log("Escorpio");
+    }
+    if (mes == 10 && dia >= 24) {
+      console.log("Escorpio");
+    }
+  };
+  let geminis = function (dia, mes) {
+    if (mes == 6 && dia <= 21) {
+      console.log("Geminis");
+    }
+    if (mes == 5 && dia >= 22) {
+      console.log("Geminis");
+    }
+  };
+  let sagitario = function (dia, mes) {
+    if (mes == 12 && dia <= 21) {
+      console.log("Sagitario");
+    }
+    if (mes == 11 && dia >= 23) {
+      console.log("Sagitario");
+    }
+  };
+  let cancer = function (dia, mes) {
+    if (mes == 7 && dia <= 23) {
+      console.log("Cancer");
+    }
+    if (mes == 6 && dia >= 21) {
+      console.log("Cancer");
+    }
+  };
+  let capricornio = function (dia, mes) {
+    if (mes == 1 && dia <= 20) {
+      console.log("Capricornio");
+    }
+    if (mes == 12 && dia >= 22) {
+      console.log("Capricornio");
+    }
+  };
+  let leo = function (dia, mes) {
+    if (mes == 8 && dia <= 23) {
+      console.log("Leo");
+    }
+    if (mes == 7 && dia >= 24) {
+      console.log("Leo");
+    }
+  };
+  let acuario = function (dia, mes) {
+    if (mes == 2 && dia <= 19) {
+      console.log("Acuario");
+    }
+    if (mes == 1 && dia >= 21) {
+      console.log("Acuario");
+    }
+  };
+  let virgo = function (dia, mes) {
+    if (mes == 9 && dia <= 23) {
+      console.log("Virgo");
+    }
+    if (mes == 8 && dia >= 24) {
+      console.log("Virgo");
+    }
+  };
+  let piscis = function (dia, mes) {
+    if (mes == 3 && dia <= 20) {
+      console.log("Piscis");
+    }
+    if (mes == 2 && dia >= 20) {
+      console.log("Piscis");
+    }
+  };
+
+  let lista = [
+    aries(dia, mes),
+    libra(dia, mes),
+    tauro(dia, mes),
+    scorpio(dia, mes),
+    geminis(dia, mes),
+    sagitario(dia, mes),
+    cancer(dia, mes),
+    capricornio(dia, mes),
+    leo(dia.mes),
+    acuario(dia, mes),
+    virgo(dia, mes),
+    piscis(dia, mes),
+  ];
+  lista.forEach((m) => (dia, mes));
 }
+
+getSignoZodiaco("23/04");
+
+getSignoZodiaco("15/04");
+
+getSignoZodiaco("31/10");
