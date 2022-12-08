0b75c2f9bd6fcdfba0d2a6ce2d902e9cf721d038
ErikPC <epardillo@cifpfbmoll.eu>
2022-12-08 13:03:02 +0100

    fix: hacer act5

--- activitat_js_bloc_2/README.md
+++ activitat_js_bloc_2/README.md
@@ -46,3 +46,10 @@ usar un diccionario, array o lista que asocia a cada signo el período del año 
 
 Actividad C4
 Escribe un programa que muestre una tabla de multiplicar como la siguiente:
+
+Actividad C5
+Un viajero desea saber cuánto tiempo tomó un viaje que realizó. Él tiene la duración en
+minutos de cada uno de los tramos del viaje.
+Desarrolla un programa que permita ingresar los tiempos de viaje de los tramos y entregue
+como resultado el tiempo total de viaje en formato horas:minutos
+El programa deja de pedir tiempos de viaje cuando se ingresa un 0.
--- activitat_js_bloc_2/actc4/codigo.js
+++ activitat_js_bloc_2/actc4/codigo.js
@@ -1,65 +1,19 @@
-var tabla1 = "";
-for (let i = 0; i < 11; i++) {
-  tabla1 = tabla1 + " " + toString(i);
+var horas = 0;
+var min = 0;
+var i = true;
+while (i > 0) {
+  i = prompt("tiempo tramo en min");
+  if (i > 60) {
+    horas = horas + i / 60;
+    min = i % 60;
+  } else {
+    min = min + i;
+  }
+  console.log(
+    "El viaje a durado " +
+      toString(horas) +
+      " horas y " +
+      toString(min) +
+      " minutos."
+  );
 }
-var tabla2 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 2;
-  tabla2 = tabla2 + " " + toString(result);
-}
-var tabla3 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 3;
-  tabla3 = tabla3 + " " + toString(result);
-}
-var tabla4 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 4;
-  tabla4 = tabla4 + " " + toString(result);
-}
-var tabla5 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 5;
-  tabla5 = tabla5 + " " + toString(result);
-}
-var tabla6 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 6;
-  tabla6 = tabla6 + " " + toString(result);
-}
-var tabla7 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 7;
-  tabla7 = tabla7 + " " + toString(result);
-}
-var tabla8 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 8;
-  tabla8 = tabla8 + " " + toString(result);
-}
-var tabla8 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 8;
-  tabla8 = tabla8 + " " + toString(result);
-}
-var tabla9 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 9;
-  tabla9 = tabla9 + " " + toString(result);
-}
-var tabla10 = "";
-for (let i = 0; i < 11; i++) {
-  let result = i * 10;
-  tabla10 = tabla10 + " " + toString(result);
-}
-
-console.log(tabla1);
-console.log(tabla2);
-console.log(tabla3);
-console.log(tabla4);
-console.log(tabla5);
-console.log(tabla6);
-console.log(tabla7);
-console.log(tabla8);
-console.log(tabla9);
-console.log(tabla10);
