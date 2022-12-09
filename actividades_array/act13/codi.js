819099d172d4d18425458e8c9ee2c53dc9f61188
ErikPC <epardillo@cifpfbmoll.eu>
2022-12-09 23:50:20 +0100

    feat: acabar act13 de js array

---
+++ actividades_array/act13/codi.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5];
+let suma = 0;
+
+// recorremos cada uno de los elementos del array 'numeros' y los sumamos
+numeros.forEach((n) => {
+  suma += n;
+});
+
+console.log(suma);
